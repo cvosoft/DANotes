6b9c237c6ed7dd25a9ca022d3198f73a48fd382d
Christoph Völker <christov@gmx.de>
2024-12-17 07:56:32 +0100

    update

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, collectionData, doc, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  items$;
+
+  unsubList;
+  //unsubSingle;
+
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+
+      })
+    })
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getSingleDocRef(collId: string, docId: string) {
+    return doc(collection(this.firestore, collId), docId)
+  }
+
+
 }
